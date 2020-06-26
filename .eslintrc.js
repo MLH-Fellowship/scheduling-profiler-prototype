0f94e6390389df603a0af26e2b068f416f3aee14
E-Liang Tan <eliang@eliangtan.com>
2020-06-26 14:20:27 +0800

    Delete all irrelevant ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const {
-  es5Paths,
-  esNextPaths,
-} = require('./scripts/shared/pathsByLanguageVersion');
-
 const restrictedGlobals = require('confusing-browser-globals');
 
 const OFF = 0;
@@ -16,12 +11,12 @@ module.exports = {
   // Stop ESLint from looking for a configuration file in parent folders
   root: true,
 
-  plugins: ['jest', 'no-for-of-loops', 'react', 'react-internal'],
+  plugins: ['jest', 'no-for-of-loops', 'react'],
 
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 8,
-    sourceType: 'script',
+    sourceType: 'module',
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
     },
@@ -56,12 +51,9 @@ module.exports = {
     'space-before-function-paren': OFF,
     'valid-typeof': [ERROR, {requireStringLiterals: true}],
 
-    // We apply these settings to files that should run on Node.
-    // They can't use JSX or ES6 modules, and must be in strict mode.
-    // They can, however, use other ES6 features.
-    // (Note these rules are overridden later for source files.)
     'no-var': ERROR,
-    strict: ERROR,
+    'prefer-const': ERROR,
+    strict: OFF,
 
     // Enforced by Prettier
     // TODO: Prettier doesn't handle long strings or long comments. Not a big
@@ -90,47 +82,9 @@ module.exports = {
     // Prevent for...of loops because they require a Symbol polyfill.
     // You can disable this rule for code that isn't shipped (e.g. build scripts and tests).
     'no-for-of-loops/no-for-of-loops': ERROR,
-
-    // CUSTOM RULES
-    // the second argument of warning/invariant should be a literal string
-    'react-internal/no-primitive-constructors': ERROR,
-    'react-internal/no-to-warn-dev-within-to-throw': ERROR,
-    'react-internal/invariant-args': ERROR,
-    'react-internal/warning-args': ERROR,
-    'react-internal/no-production-logging': ERROR,
-    'react-internal/no-cross-fork-imports': ERROR,
   },
 
   overrides: [
-    {
-      // We apply these settings to files that we ship through npm.
-      // They must be ES5.
-      files: es5Paths,
-      parser: 'espree',
-      parserOptions: {
-        ecmaVersion: 5,
-        sourceType: 'script',
-      },
-      rules: {
-        'no-var': OFF,
-        strict: ERROR,
-      },
-    },
-    {
-      // We apply these settings to the source files that get compiled.
-      // They can use all features including JSX (but shouldn't use `var`).
-      files: esNextPaths,
-      parser: 'babel-eslint',
-      parserOptions: {
-        ecmaVersion: 8,
-        sourceType: 'module',
-      },
-      rules: {
-        'no-var': ERROR,
-        'prefer-const': ERROR,
-        strict: OFF,
-      },
-    },
     {
       files: ['**/__tests__/*.js'],
       rules: {
@@ -140,45 +94,5 @@ module.exports = {
         'jest/valid-expect-in-promise': ERROR,
       },
     },
-    {
-      files: [
-        '**/__tests__/**/*.js',
-        'scripts/**/*.js',
-        'packages/*/npm/**/*.js',
-        'packages/dom-event-testing-library/**/*.js',
-        'packages/react-devtools*/**/*.js',
-      ],
-      rules: {
-        'react-internal/no-production-logging': OFF,
-        'react-internal/warning-args': OFF,
-      },
-    },
-    {
-      files: ['packages/react-native-renderer/**/*.js'],
-      globals: {
-        nativeFabricUIManager: true,
-      },
-    },
-    {
-      files: ['packages/react-transport-dom-webpack/**/*.js'],
-      globals: {
-        __webpack_chunk_load__: true,
-        __webpack_require__: true,
-      },
-    },
   ],
-
-  globals: {
-    SharedArrayBuffer: true,
-
-    spyOnDev: true,
-    spyOnDevAndProd: true,
-    spyOnProd: true,
-    __PROFILE__: true,
-    __UMD__: true,
-    __EXPERIMENTAL__: true,
-    __VARIANT__: true,
-    gate: true,
-    trustedTypes: true,
-  },
 };
