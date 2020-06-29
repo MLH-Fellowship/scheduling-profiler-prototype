ce99015b2713e7dbdc29fe69c9d5b4043565e832
E-Liang Tan <eliang@eliangtan.com>
2020-06-29 14:45:26 +0800

    Extract automatic ImportPage from App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,45 @@
 // @flow
 
-import type {TimelineEvent} from './speedscope/import/chrome';
 import type {FlamechartData, ReactProfilerData} from './types';
 
-import React, {useEffect, useState} from 'react';
-import {unstable_batchedUpdates} from 'react-dom';
+import React, {useState, useCallback} from 'react';
 import CanvasPage from './CanvasPage';
 
-import {REACT_PRIORITIES} from './canvas/constants';
 import {getPriorityHeight} from './canvas/canvasUtils';
-import preprocessData from './util/preprocessData';
-import preprocessFlamechart from './util/preprocessFlamechart';
-
-// TODO: Add import button but keep a static path until canvas layout is ready
-import JSON_PATH from 'url:../static/small-devtools.json';
+import ImportPage from './ImportPage';
+import {REACT_PRIORITIES} from './canvas/constants';
 
 export default function App() {
-  const [data, setData] = useState<ReactProfilerData | null>(null);
+  const [profilerData, setProfilerData] = useState<ReactProfilerData | null>(
+    null,
+  );
   const [flamechart, setFlamechart] = useState<FlamechartData | null>(null);
   const [schedulerCanvasHeight, setSchedulerCanvasHeight] = useState<number>(0);
 
-  useEffect(() => {
-    fetch(JSON_PATH)
-      .then(res => res.json())
-      .then((events: TimelineEvent[]) => {
-        // Filter null entries and sort by timestamp.
-        // I would not expect to have to do either of this,
-        // but some of the data being passed in requires it.
-        events = events.filter(Boolean).sort((a, b) => (a.ts > b.ts ? 1 : -1));
+  const handleDataImported = useCallback(
+    (
+      importedProfilerData: ReactProfilerData,
+      importedFlamechart: FlamechartData,
+    ) => {
+      setSchedulerCanvasHeight(
+        REACT_PRIORITIES.reduce((height, priority) => {
+          return height + getPriorityHeight(importedProfilerData, priority);
+        }, 0),
+      );
+      setProfilerData(importedProfilerData);
+      setFlamechart(importedFlamechart);
+    },
+  );
 
-        if (events.length > 0) {
-          unstable_batchedUpdates(() => {
-            const processedData = preprocessData(events);
-            setData(processedData);
-
-            setFlamechart(preprocessFlamechart(events));
-
-            let height = 0;
-
-            REACT_PRIORITIES.forEach(priority => {
-              height += getPriorityHeight(processedData, priority);
-            });
-
-            setSchedulerCanvasHeight(height);
-          });
-        }
-      });
-  }, []);
-
-  if (data && flamechart) {
+  if (profilerData && flamechart) {
     return (
       <CanvasPage
-        profilerData={data}
+        profilerData={profilerData}
         flamechart={flamechart}
         schedulerCanvasHeight={schedulerCanvasHeight}
       />
     );
   } else {
-    return <div>Loading</div>;
+    return <ImportPage onDataImported={handleDataImported} />;
   }
 }
