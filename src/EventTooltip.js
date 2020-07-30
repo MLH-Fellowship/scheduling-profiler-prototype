6f3fc3bcb79624c0775b56759eb6b3f94752e577
Kartik Choudhary <kartikc.918@gmail.com>
2020-07-30 21:46:48 +0530

    Move functions to EventTooltip.js

--- src/EventTooltip.js
+++ src/EventTooltip.js
@@ -15,7 +15,6 @@ import React, {Fragment, useRef} from 'react';
 import {COLORS} from './canvas/constants';
 import {getBatchRange} from './util/getBatchRange';
 import useSmartTooltip from './util/useSmartTooltip';
-import {getReactEventLabel, getReactMeasureLabel} from './util/tooltipLabel';
 import styles from './EventTooltip.css';
 
 type Props = {|
@@ -37,6 +36,42 @@ function trimComponentName(name) {
     return name.substring(0, 127) + '...';
   }
   return name;
+}
+
+function getReactEventLabel(type) {
+  switch (type) {
+    case 'schedule-render':
+      return 'render scheduled';
+    case 'schedule-state-update':
+      return 'state update scheduled';
+    case 'schedule-force-update':
+      return 'force update scheduled';
+    case 'suspense-suspend':
+      return 'suspended';
+    case 'suspense-resolved':
+      return 'suspense resolved';
+    case 'suspense-rejected':
+      return 'suspense rejected';
+    default:
+      return null;
+  }
+}
+
+function getReactMeasureLabel(type: string) {
+  switch (type) {
+    case 'commit':
+      return 'commit';
+    case 'render-idle':
+      return 'idle';
+    case 'render':
+      return 'render';
+    case 'layout-effects':
+      return 'layout effects';
+    case 'passive-effects':
+      return 'passive effects';
+    default:
+      return null;
+  }
 }
 
 export default function EventTooltip({data, hoveredEvent, origin}: Props) {
