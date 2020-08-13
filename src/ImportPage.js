dad09782617b5a2bcbb84a7c78bfc6e595584d6c
Kartik Choudhary <kartikc.918@gmail.com>
2020-08-13 17:05:42 +0530

    Re-do landing, add guide overlay, add spinner

--- src/ImportPage.js
+++ src/ImportPage.js
@@ -3,7 +3,7 @@
 import type {TimelineEvent} from '@elg/speedscope';
 import type {ReactProfilerData} from './types';
 
-import React, {useEffect, useCallback, useRef} from 'react';
+import React, {useEffect, useState, useCallback, useRef} from 'react';
 import profilerBrowser from './assets/profilerBrowser.png';
 import style from './ImportPage.css';
 
@@ -28,15 +28,16 @@ export default function ImportPage({onDataImported}: Props) {
   );
 
   // DEV only: auto-import a demo profile on component mount (i.e. page load)
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') return;
-    fetch(JSON_PATH)
-      .then(res => res.json())
-      .then(processTimeline);
-  }, [processTimeline]);
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV === 'production') return;
+  //   fetch(JSON_PATH)
+  //     .then(res => res.json())
+  //     .then(processTimeline);
+  // }, [processTimeline]);
 
   const handleProfilerInput = useCallback(
     async (event: File) => {
+      setShowSpinner(true);
       const readFile = await readInputData(event.target.files[0]);
       processTimeline(JSON.parse(readFile));
     },
@@ -44,6 +45,11 @@ export default function ImportPage({onDataImported}: Props) {
   );
 
   const upload = useRef(null);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
   return (
     <div className={style.App}>
@@ -52,14 +58,35 @@ export default function ImportPage({onDataImported}: Props) {
           <div className={style.cardcontainer}>
             <div className={style.row}>
               <div className={style.column}>
-                <img
-                  src={profilerBrowser}
-                  className={style.browserScreenshot}
-                  alt="logo"
-                />
+                {showSpinner ? (
+                  <>
+                    <div className={style.loader}>Loading...</div>
+                    <p className={style.subtext}>
+                      (Loading profile, it may seem like the page is frozen)
+                    </p>
+                  </>
+                ) : (
+                  <img
+                    src={profilerBrowser}
+                    className={style.browserScreenshot}
+                    alt="logo"
+                  />
+                )}
               </div>
               <div className={style.columncontent}>
-                <h2>React Concurrent Mode Profiler</h2>
+                <h2>
+                  <span className={style.header}>
+                    React Concurrent Mode Profiler
+                  </span>
+                  <a href="https://github.com/MLH-Fellowship/scheduling-profiler-prototype">
+                    <svg width="24" height="24" viewBox="0 0 16 16">
+                      <path
+                        fillRule="evenodd"
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                      />
+                    </svg>
+                  </a>
+                </h2>
                 <hr />
                 <p>
                   Import a captured{' '}
@@ -70,24 +97,9 @@ export default function ImportPage({onDataImported}: Props) {
                   </a>{' '}
                   from Chrome Devtools.
                   <br />
-                  To zoom, scroll while holding down <kbd>Ctrl</kbd> or{' '}
-                  <kbd>Shift</kbd>
-                </p>
-                <p className={style.legendKey}>
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#ff718e" />
-                  </svg>
-                  State Update Scheduled
                   <br />
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#9fc3f3" />
-                  </svg>
-                  State Update Scheduled
-                  <br />
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#a6e59f" />
-                  </svg>
-                  Suspended
+                  According to all known laws of aviation, there is no way a bee
+                  should be able to fly.
                 </p>
 
                 <div className={style.buttongrp}>
@@ -105,11 +117,81 @@ export default function ImportPage({onDataImported}: Props) {
                       accept="application/json"
                     />
                   </label>
-                  <a href="https://github.com/MLH-Fellowship/scheduling-profiler-prototype">
+                  <a onClick={toggleModal}>
                     <button className={style.btndoc}>
-                      <span>Source </span>
+                      <span>How-to </span>
                     </button>
                   </a>
+                </div>
+                <div
+                  className={`${style.modalOverlay} ${
+                    showModal ? style.active : ''
+                  }`}>
+                  <div
+                    className={`${style.modal}  ${
+                      showModal ? style.active : ''
+                    }`}>
+                    <a onClick={toggleModal} className={style.closeModal}>
+                      <svg viewBox="0 0 20 20">
+                        <path
+                          fill="#000000"
+                          d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"
+                        />
+                      </svg>
+                    </a>
+                    {/* Modal Content */}
+                    <div className={style.modalContent}>
+                      <div className={style.cardcontainer}>
+                        <div className={style.columncontent}>
+                          <h2>
+                            React Concurrent Mode Profiler
+                            <svg
+                              width="24"
+                              height="24"
+                              className={style.githubLogo}
+                              viewBox="0 0 16 16">
+                              <path
+                                fill-rule="evenodd"
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                              />
+                            </svg>
+                          </h2>
+                          <hr />
+                          <p>
+                            Import a captured{' '}
+                            <a
+                              className={style.link}
+                              href="https://developers.google.com/web/tools/chrome-devtools/evaluate-performance">
+                              performance profile
+                            </a>{' '}
+                            from Chrome Devtools.
+                            <br />
+                            <br />
+                            To zoom, scroll while holding down <kbd>
+                              Ctrl
+                            </kbd>{' '}
+                            or <kbd>Shift</kbd>
+                          </p>
+                          <p className={style.legendKey}>
+                            <svg height="20" width="20">
+                              <circle cx="10" cy="10" r="5" fill="#ff718e" />
+                            </svg>
+                            State Update Scheduled
+                            <br />
+                            <svg height="20" width="20">
+                              <circle cx="10" cy="10" r="5" fill="#9fc3f3" />
+                            </svg>
+                            State Update Scheduled
+                            <br />
+                            <svg height="20" width="20">
+                              <circle cx="10" cy="10" r="5" fill="#a6e59f" />
+                            </svg>
+                            Suspended
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
